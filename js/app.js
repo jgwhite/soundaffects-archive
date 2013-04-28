50cb2f914d9a55d311cb69a89c6c8f02593d0b60
Jamie White <jamie@jgwhite.co.uk>
2013-04-28 17:10:06 +0100

    Some styling and return to function

--- js/app.js
+++ js/app.js
@@ -20,62 +20,49 @@ App.MaterialsRoute = Ember.Route.extend({
   }
 });
 
-App.MaterialRoute = Ember.Route.extend({
-  model: function(params) {
-    return App.Material.find(params.material_id);
-  }
+// --- Helpers ---
+
+Ember.Handlebars.registerBoundHelper('unescape', function(string) {
+  return unescape(string);
 });
 
-// --- Controllers ---
-
-App.MaterialsController = Ember.ArrayController.extend({
-  isLoadedBinding: Ember.Binding.oneWay('content.isLoaded')
-});
-
-App.MaterialController = Ember.ObjectController.extend({
-  isLoaded: Ember.K
+Ember.Handlebars.registerBoundHelper('thisYear', function() {
+  return new Date().getFullYear();
 });
 
 // --- Models ---
 
-App.Material = Ember.ObjectProxy.extend({
-  isLoaded: function() {
-    return this.get('store.isLoaded');
-  }.property('store.isLoaded'),
-
-  content: function() {
-    var store = this.get('store'),
-        data = store.findById(this.get('id')) || {};
-
-    return data;
-  }.property('isLoaded')
-});
+App.Material = Ember.Object.extend();
 
 App.Material.find = function(id) {
-  App.Material.__store__ = App.Material.__store__ || App.Store.create();
-  var store = App.Material.__store__;
-
   if (Ember.isNone(id)) {
-    return store;
+    return App.store.all();
   } else {
-    return App.Material.create({ id: id, store: store });
+    return App.store.findById(id);
   }
 }
 
-App.Store = Ember.ArrayProxy.extend({
+// --- Store ---
+
+App.Store = Ember.Object.extend({
   url: 'https://docs.google.com/spreadsheet/pub?' +
        'key=0Aqx7vtK6RxxZdEZ4emYyd0ljNXRfcnNEM3FzLUhueGc' +
        '&output=csv',
 
-  isLoaded: false,
+  content: [],
+  idMap: {},
 
   init: function() {
     this._super();
     this._fetchData();
   },
 
+  all: function() {
+    return this.get('content');
+  },
+
   findById: function(id) {
-    return this.findProperty('id', id);
+    return this._objectForId(id);
   },
 
   _fetchData: function() {
@@ -83,8 +70,32 @@ App.Store = Ember.ArrayProxy.extend({
 
     $.get(this.get('url')).then(function(csv) {
       var data = $.csv.toObjects(csv);
-      self.set('content', data);
-      self.set('isLoaded', true);
+      data.forEach(function(datum) { self._addDatum(datum) });
     });
+  },
+
+  _addDatum: function(datum) {
+    var object = this._objectForId(datum.id);
+
+    object.setProperties({
+      id: datum.id,
+      name: datum.name,
+      url: datum.url,
+      album: datum.album,
+      type: datum.type,
+      keyStage: datum.keyStage,
+      countries: datum.countries,
+      subjects: datum.subjects,
+      isLoaded: true
+    });
+
+    this.get('content').addObject(object);
+  },
+
+  _objectForId: function(id) {
+    var idMap = this.get('idMap');
+    return idMap[id] = idMap[id] || App.Material.create({ id: id });
   }
 });
+
+App.store = App.Store.create();
